3c0dd7fb10f5adb71fc8cb54e67bd6ab4a481e26
TAMIRES ZIM <tamires_zim@estudante.sesisenai.org.br>
2024-05-21 20:54:48 -0300

    Meu Primeiro Commit

---
+++ Aula01.js
@@ -0,0 +1,5 @@
+console.log("Olá Mundo")
+var media = (10 + 7 + 5) / 3
+
+console.log(media)
+prompt ("Testando")
---
+++ Aula02.js
@@ -0,0 +1,17 @@
+var qg = prompt("Digite a qtd de gols")
+var qc = prompt("Digite a Qtd de Passes Certos")
+var qe = prompt("Digite a Qtd de Passes Errado")
+
+var pontos = (qg * 50) + (qe * 10) + (qc * - 5);
+    alert("Total de Pontuação: " + pontos)
+if(pontos <50){
+    alert("Péssima Partida");
+}else if(pontos>= 50 && pontos <100){
+    alert("Partida Ruim");
+}else if(pontos>= 100 && pontos < 150){
+    alert("Fez o Básico");
+}else if(pontos >= 150 && pontos < 200){
+    alert("Foi bem na Partida");
+}else{
+    alert("Jogou Demais");
+}
